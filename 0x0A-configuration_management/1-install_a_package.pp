5fc382555657fc26555a2132aa49fb07ded6bb08
Pury-01 <puryokumu@gmail.com>
2024-07-03 14:02:23 +0300

    added werkzeug installion

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install flask from pip3
 
-package {'python3':
+package { 'python3':
   ensure =>  installed,
 }
 
@@ -8,8 +8,8 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.3',
   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
   require => Package['python3-pip'],
 }

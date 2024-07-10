a533901cdc8b6da8fe42b6b5389b6438d697df16
Pury-01 <puryokumu@gmail.com>
2024-07-10 15:26:25 +0300

    configuring your server with Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,17 +4,36 @@ package { 'nginx':
   ensure => installed,
 }
 
-file { 'etc/nginx/sites-available/default':
-  ensure => 'file',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/@pury8351 permanent;',
-}
-
 file { '/var/www/html/index.html':
+  ensure  => file,
   content => 'Hello World!',
 }
 
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location = /redirect_me {
+        return 301 https://www.youtube.com/@pury8351;
+    }
+
+    location = / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+  ",
+  notify  => Service[ 'nginx' ],
+}
+
 service { 'nginx':
-  ensure  => running,
-  require => Package[ 'nginx' ],
+  ensure => running,
+  enable => true,
 }

91f50ff25fd9ffbfa4adb4665fd3bec9511340b3
Pury-01 <puryokumu@gmail.com>
2024-08-14 09:47:52 +0300

    added python version

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "python:subreddit.subscribers.count:v3.4.3"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()

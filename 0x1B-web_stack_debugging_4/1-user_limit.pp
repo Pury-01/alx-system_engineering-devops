d273d71c9f84e79489931da91e8b730eb9a657c4
Pury-01 <puryokumu@gmail.com>
2024-08-21 15:00:48 +0300

    Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# change Os configuration so that it is possible to login with the holberton user
+# and open a file without any error message
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.config',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
+
